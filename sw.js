a3141b9ce923d8bae65fbd969bd6434be391bcfe
wilsondain623-cmd <wilsondain623@gmail.com>
2026-07-09 10:11:35 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const C = "dwil-spotter-cache-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(C).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
